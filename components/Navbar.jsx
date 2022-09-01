c70e2c2e8cbbad467376dd534815151ebe80d0b9
10afrika <10@afrika.dev>
2022-09-01 13:25:20 +0300

    Navbar modified

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -26,19 +26,19 @@ const Navbar = () => {
                 <div>
                     <ul className='mr-20 hidden md:flex text-white'>
                         <Link href='/'>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b  hover:text-gray-400'>Home</li>
                         </Link>
                         <Link href='/'>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>City Weather</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b  hover:text-gray-400'>City Weather</li>
                         </Link>
                         <Link href='/'>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>Hourly Weather</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b  hover:text-gray-400'>Hourly Weather</li>
                         </Link><Link href='/'>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                            <li className='ml-10 text-sm uppercase hover:border-b  hover:text-gray-400'>About</li>
                         </Link>
                     </ul>
                     <div onClick={handleToggle} className='md:hidden'>
-                        <AiOutlineMenu size={25} />
+                        <AiOutlineMenu size={25} className='text-white' />
                     </div>
                 </div>
             </div>
@@ -46,10 +46,9 @@ const Navbar = () => {
                 <div className={nav ? 'fixed left-0 top-0 width-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] ease-in duration-500 ' : 'fixed left-[-100%] top-0  ease-in duration-500 '}>
                     <div className='mt-[10px] ml-[20px] mr-[10px]'>
                         <div className='flex w-full items-center justify-between '>
-                            <span className=' flex  items-center justify-center w-10 h-10 bg-white rounded-full'>
-                                <WiDayFog className="inline-block w-8 h-8 bg-transparent rounded-full text-black" />
-
-                            </span>
+                            <div className='rounded-full bg-white shadow-s shadow-white p-3 mb-2 mt-2'>
+                                <WiDayFog className='text-black' />
+                            </div>
                             <div onClick={handleToggle} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                                 <AiOutlineClose />
                             </div>
