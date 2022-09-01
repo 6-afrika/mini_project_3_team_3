46ee5dc1ef636c93c597363810428617fa1342ed
10afrika <10@afrika.dev>
2022-09-01 14:04:46 +0300

    Hero Section Added

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import MainImage from '../public/assets/MainImage.png'
+import Image from 'next/image'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section class="relative mt-40 ml-20 md:ml-10 sm:ml-0 sm:mr-10">
+            <div class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28">
+                <div class="flex flex-1 flex-col items-center lg:items-start">
+                    <h2 class="text-bookmark-blue text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
+                        A Simple Weather Application
+                    </h2>
+                    <p class="text-bookmark-grey text-lg text-center lg:text-left mb-6">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc imperdiet nisl eu justo bibendum fringilla maximus luctus magna. Praesent urna dolor, dapibus ut blandit pellentesque, fringilla lobortis nulla. Ut bibendum turpis ac est condimentum auctor. Mauris eu sapien quis lorem tristique luctus. Integer quis nibh cursus, consectetur magna placerat, pulvinar sem. Morbi semper feugiat tempor. Vestibulum vitae turpis lacinia, euismod est in, blandit lacus. Mauris sed est in lectus ultrices vestibulum.
+
+                    </p>
+
+                </div>
+                <div class="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0 z-10">
+                    <Image className="" src={MainImage} width={400} height={400} alt="" />
+                </div>
+            </div>
+
+        </section>
+
     )
 }
 
